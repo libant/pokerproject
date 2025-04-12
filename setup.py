0062125948a71fae302e84963f61d2f171786f5b
libant <liban.timir@mail.utoronto.ca>
2025-04-12 00:00:39 -0400

    project near near complete

--- setup.py
+++ setup.py
@@ -1,1 +1,13 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="bayesian-poker-inference",
+    version="0.1.0",
+    description="A Bayesian inference module for estimating poker hand types from partial information.",
+    author="Liban Timir",
+    packages=find_packages(),
+    install_requires=[
+        "matplotlib",
+    ],
+)
 
